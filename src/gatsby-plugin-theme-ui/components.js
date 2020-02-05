3e88041a16fb0e8078ab55a010dc962ba57ff132
Vladimir <vladimir.skok@bvaccel.com>
2020-02-04 23:09:27 -0800

    refactor: pre component

--- src/gatsby-plugin-theme-ui/components.js
+++ src/gatsby-plugin-theme-ui/components.js
@@ -1,27 +1,25 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui'
 import Code from '../components/code'
+import { isPreWithCodeBlock } from '../utils/is-pre-with-code-block'
 
 export default {
-  pre: preProps => {
-    if (
-      preProps &&
-      preProps.children &&
-      preProps.children.props
-    ) {
-      if (
-        preProps.children.props.className &&
-        preProps.children.props.className.includes('language-')
-      ) {
-        console.log('code tag', preProps)
-        return <pre {...preProps}><Code {...preProps.children.props}>{preProps.children}</Code></pre>
-        } else {
-          console.log('pre tag', preProps)
-        return <pre {...preProps}>{preProps.children.props.children}</pre>
-        }
-      } else {
-      return null
-    }
-  },
   code: Code,
+  pre: preProps => {
+    if (isPreWithCodeBlock(preProps)) {
+      return (
+        <pre {...preProps}>
+          <Code {...preProps.children.props}>
+            {preProps.children}
+          </Code>
+        </pre>
+      )
+    } else {
+      return (
+        <pre {...preProps}>
+          {preProps.children.props.children}
+        </pre>
+      )
+    }
+  }
 }
---
+++ src/utils/is-pre-with-code-block.js
@@ -0,0 +1,13 @@
+exports.isPreWithCodeBlock = preProps => {
+  if (
+    preProps.children &&
+    preProps.children.props &&
+    preProps.children.props.mdxType === `code` &&
+    preProps.children.props.className &&
+    preProps.children.props.className.includes(`language-`)
+  ) {
+    return true
+  } else {
+    return false
+  }
+}
