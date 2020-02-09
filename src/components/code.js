4052d5950bd9605e4004796958f6cd9b3ec0d82c
Vladimir <vladimir.skok@bvaccel.com>
2020-02-08 23:37:10 -0800

    feat: update react-live in code component

--- src/components/code.js
+++ src/components/code.js
@@ -1,6 +1,13 @@
+/** @jsx jsx */
+import { jsx } from 'theme-ui'
 import React from 'react'
 import { defaultProps } from 'prism-react-renderer'
-import ReactLive from './react-live'
+import {
+  LiveProvider,
+  LiveEditor,
+  LiveError,
+  LivePreview
+} from 'react-live'
 import PrismHighlight from './prism-highlight'
 import calculateLinesToHighlight from '../utils/calculate-lines-to-highlight'
 
@@ -11,12 +18,12 @@ const aliases = {
 
 const Code = ({
   children,
-  className: outerClassName,
+  className,
   title,
   metastring,
   ...props
 }) => {
-  const [language] = outerClassName.replace(/language-/, '').split(' ')
+  const [language] = className.replace(/language-/, '').split(' ')
   const lang = aliases[language] || language
   const shouldHighlightLine = calculateLinesToHighlight(metastring)
   const isReactLive = props['react-live'] ? true : false
@@ -26,18 +33,23 @@ const Code = ({
     <>
       {
         isReactLive ?
-        <ReactLive
+        <LiveProvider
           code={codeString}
           noInline={false}
-        /> :
+          theme={undefined}
+        >
+          <LiveEditor />
+          <LiveError />
+          <LivePreview />
+        </LiveProvider> :
         <PrismHighlight
-          defaultProps={defaultProps}
-          props={props}
-          outerClassName={outerClassName}
           code={codeString}
           language={lang}
+          outerClassName={className}
           theme={undefined}
           shouldHighlightLine={shouldHighlightLine}
+          defaultProps={defaultProps}
+          props={props}
         />
       }
     </>
--- src/components/prism-highlight.js
+++ src/components/prism-highlight.js
@@ -5,56 +5,55 @@ import Highlight from 'prism-react-renderer'
 const PrismHighlight = ({
   defaultProps,
   props,
-  outerClassName,
+  // outerClassName,
   code,
   language,
+  theme,
   shouldHighlightLine
 }) => (
   <Highlight
-    {...defaultProps}
-    {...props}
     code={code}
     language={language}
-    theme={undefined}
+    theme={theme}
+    {...defaultProps}
+    {...props}
   >
     {({
-      className,
-      style,
+      // className,
+      // style,
       tokens,
       getLineProps,
       getTokenProps
     }) => (
       <Styled.code
-        className={`${outerClassName} ${className}`}
-        style={style}
+        // className={`${outerClassName} ${className}`}
+        // style={style}
       >
-        {tokens.map((line, i) => {
-          return (
-            <div
-              key={i}
-              {...getLineProps({
-                line,
-                key: i,
-                className: shouldHighlightLine(i) ? 'highlight-line' : '',
-              })}
-            >
-              <span sx={{
-                display: 'inline-block',
-                width: '1.8em',
-                userSelect: 'none',
-                opacity: 0.3
-              }}>
-                {i + 1}
-              </span>
-              {line.map((token, key) => (
-                <span
-                  {...getTokenProps({ token, key })}
-                  sx={{ display: 'inline-block' }}
-                />
-              ))}
-            </div>
-          )
-        })}
+        {tokens.map((line, i) => (
+          <div
+            key={i}
+            {...getLineProps({
+              line,
+              key: i,
+              className: shouldHighlightLine(i) ? 'highlight-line' : '',
+            })}
+          >
+            <span sx={{
+              display: 'inline-block',
+              width: '1.8em',
+              userSelect: 'none',
+              opacity: 0.3
+            }}>
+              {i + 1}
+            </span>
+            {line.map((token, key) => (
+              <span
+                {...getTokenProps({ token, key })}
+                sx={{ display: 'inline-block' }}
+              />
+            ))}
+          </div>
+        ))}
       </Styled.code>
     )}
   </Highlight>
