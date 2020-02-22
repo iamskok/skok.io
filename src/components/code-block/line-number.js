1f534d3f18b00ddcde0457066a53f9d76dbd69b0
Vladimir <vladimir.skok@bvaccel.com>
2020-02-22 13:50:11 -0800

    add code line number component

---
+++ src/components/code-block/line-number.js
@@ -0,0 +1,17 @@
+/** @jsx jsx */
+import { jsx } from 'theme-ui'
+
+const LineNumber = ({ index }) => (
+  <span sx={{
+    display: 'inline-block',
+    width: '1.8em',
+    userSelect: 'none',
+    textAlign: 'center',
+    opacity: 0.25,
+    backgroundColor: 'black',
+  }}>
+    {index + 1}
+  </span>
+)
+
+export default LineNumber
