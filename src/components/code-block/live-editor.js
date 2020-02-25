062e65afbf1750ae296eec74df41078dcb6aa944
Vladimir <vladimir.skok@bvaccel.com>
2020-02-24 20:44:59 -0800

    style: edit return statement

--- src/components/code-block/live-editor.js
+++ src/components/code-block/live-editor.js
@@ -11,18 +11,16 @@ export default function LiveEditor(props) {
         theme,
         disabled,
         onChange
-      }) => {
-        return (
-          <Editor
-            theme={theme}
-            code={code}
-            language={language}
-            disabled={disabled}
-            onChange={onChange}
-            {...props}
-          />
-        )
-      }}
+      }) => (
+        <Editor
+          theme={theme}
+          code={code}
+          language={language}
+          disabled={disabled}
+          onChange={onChange}
+          {...props}
+        />
+      )}
     </LiveContext.Consumer>
   )
 }
