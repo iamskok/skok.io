78d5d6b8dc28b1c3635eccb254f34a2e0ff4881d
Vladimir <vladimir.skok@bvaccel.com>
2020-03-05 18:59:22 -0800

    refactor: styles live-editor component

--- src/components/code-block/live-editor.js
+++ src/components/code-block/live-editor.js
@@ -2,25 +2,25 @@ import React from 'react'
 import { LiveContext } from 'react-live'
 import Editor from './editor'
 
-export default function LiveEditor(props) {
-  return (
-    <LiveContext.Consumer>
-      {({
-        code,
-        language,
-        theme,
-        disabled,
-        onChange
-      }) => (
-        <Editor
-          theme={theme}
-          code={code}
-          language={language}
-          disabled={disabled}
-          onChange={onChange}
-          {...props}
-        />
-      )}
-    </LiveContext.Consumer>
-  )
-}
+const LiveEditor = props => (
+  <LiveContext.Consumer>
+    {({
+      code,
+      language,
+      theme,
+      disabled,
+      onChange
+    }) => (
+      <Editor
+        theme={theme}
+        code={code}
+        language={language}
+        disabled={disabled}
+        onChange={onChange}
+        {...props}
+      />
+    )}
+  </LiveContext.Consumer>
+)
+
+export default LiveEditor
