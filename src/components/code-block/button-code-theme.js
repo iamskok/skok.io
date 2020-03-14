25dc5b77773a8b2ce7e4b6af970029b592f6c101
Vladimir <vladimir.skok@bvaccel.com>
2020-03-14 12:39:16 -0700

    feat: rotate change theme button on click

--- src/components/code-block/button-code-theme.js
+++ src/components/code-block/button-code-theme.js
@@ -1,54 +1,65 @@
 /** @jsx jsx */
 import { jsx, Button } from 'theme-ui'
+import { useState } from 'react'
 import { PrismThemeConsumer } from './prism-theme-provider'
 
-const ButtonCodeTheme = () => (
-  <PrismThemeConsumer>
-    {({ changePrismTheme }) => (
-      <Button
-        title="Change Code Color Mode"
-        sx={{
-          cursor: 'pointer',
-          backgroundColor: 'secondary',
-          padding: 0,
-          width: 40,
-          height: 40,
-        }}
-        onClick={changePrismTheme}
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 32 32"
-          fill="currentcolor"
+const ButtonCodeTheme = () => {
+  const [isRotated, setRotated] = useState(false)
+  const rotate = () => setRotated(!isRotated)
+
+  return (
+    <PrismThemeConsumer>
+      {({ changePrismTheme }) => (
+        <Button
+          title="Change Code Color Mode"
           sx={{
-            display: 'flex',
-            margin: '0 auto'
+            cursor: 'pointer',
+            backgroundColor: 'secondary',
+            padding: 0,
+            width: 40,
+            height: 40,
+          }}
+          onClick={() => {
+            changePrismTheme();
+            rotate()
           }}
         >
-          <circle
-            cx="16"
-            cy="16"
-            r="14"
-            fill="none"
-            stroke="currentcolor"
-            stroke-width="4"
-          ></circle>
-          <path d="M 16 0 A 16 16 0 0 0 16 32 z"></path>
-        </svg>
-        <span sx={{
-          position: 'absolute',
-          top: -9999,
-          width: 1,
-          height: 1,
-          overflow: 'hidden'
-        }}>
-          Change Code Color Mode
-        </span>
-      </Button>
-    )}
-  </PrismThemeConsumer>
-)
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 32 32"
+            fill="currentcolor"
+            sx={{
+              display: 'flex',
+              margin: '0 auto',
+              transition: 'transform 400ms ease',
+              transform: isRotated ? 'rotate(180deg)' : 'rotate(0deg)'
+            }}
+          >
+            <circle
+              cx="16"
+              cy="16"
+              r="14"
+              fill="none"
+              stroke="currentcolor"
+              strokeWidth="4"
+            ></circle>
+            <path d="M 16 0 A 16 16 0 0 0 16 32 z"></path>
+          </svg>
+          <span sx={{
+            position: 'absolute',
+            top: -9999,
+            width: 1,
+            height: 1,
+            overflow: 'hidden'
+          }}>
+            Change Code Color Mode
+          </span>
+        </Button>
+      )}
+    </PrismThemeConsumer>
+  )
+}
 
 export default ButtonCodeTheme
