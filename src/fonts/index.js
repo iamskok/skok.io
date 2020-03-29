c00050d252e205fa670f491401aa1320a046e1ed
Vladimir <vladimir.skok@bvaccel.com>
2020-03-29 12:20:34 -0700

    refactor: inter font faces declarations

--- src/fonts/index.js
+++ src/fonts/index.js
@@ -1,63 +1,83 @@
 import {
-  interBlackWoff,
-  interBlackWoff2,
-  interBlackItalicWoff,
-  interBlackItalicWoff2,
-  interBoldWoff,
-  interBoldWoff2,
-  interBoldItalicWoff,
-  interBoldItalicWoff2,
-  interExtraBoldWoff,
-  interExtraBoldWoff2,
-  interExtraBoldItalicWoff,
-  interExtraBoldItalicWoff2,
-  interExtraLightWoff,
-  interExtraLightWoff2,
-  interExtraLightItalicWoff,
-  interExtraLightItalicWoff2,
-  interItalicWoff,
-  interItalicWoff2,
-  interLightWoff,
-  interLightWoff2,
-  interLightItalicWoff,
-  interLightItalicWoff2,
-  interMediumWoff,
-  interMediumWoff2,
-  interMediumItalicWoff,
-  interMediumItalicWoff2,
-  interRegularWoff,
-  interRegularWoff2,
-  interSemiBoldWoff,
-  interSemiBoldWoff2,
-  interSemiBoldItalicWoff,
-  interSemiBoldItalicWoff2,
+  // 100 and 100i
   interThinWoff,
   interThinWoff2,
   interThinItalicWoff,
   interThinItalicWoff2,
+  // 200 and 200i
+  interExtraLightWoff,
+  interExtraLightWoff2,
+  interExtraLightItalicWoff,
+  interExtraLightItalicWoff2,
+  // 300 and 300i
+  interLightWoff,
+  interLightWoff2,
+  interLightItalicWoff,
+  interLightItalicWoff2,
+  // 400 and 400i
+  interRegularWoff,
+  interRegularWoff2,
+  interItalicWoff,
+  interItalicWoff2,
+  // 500 and 500i
+  interMediumWoff,
+  interMediumWoff2,
+  interMediumItalicWoff,
+  interMediumItalicWoff2,
+  // 600 and 600i
+  interSemiBoldWoff,
+  interSemiBoldWoff2,
+  interSemiBoldItalicWoff,
+  interSemiBoldItalicWoff2,
+  // 700 and 700i
+  interBoldWoff,
+  interBoldWoff2,
+  interBoldItalicWoff,
+  interBoldItalicWoff2,
+  // 800 and 800i
+  interBlackWoff,
+  interBlackWoff2,
+  interBlackItalicWoff,
+  interBlackItalicWoff2,
+  // 900 and 900i
+  interExtraBoldWoff,
+  interExtraBoldWoff2,
+  interExtraBoldItalicWoff,
+  interExtraBoldItalicWoff2,
+  // variable
   interItalicVarWoff2,
   interRomanVarWoff2,
-  // interVarWoff2,
+  // variable experimental
+  interVarWoff2,
   // interVvarWoff2,
 } from './inter/inter'
 
-const interFontFaceStack = `
+
+const inter100 = `
   @font-face {
     font-family: 'Inter';
     font-style: normal;
     font-weight: 100;
     font-display: swap;
-    src: url(${interThinWoff2}) format("woff2"),
+    src: local('Inter Thin'),
+      url(${interThinWoff2}) format("woff2"),
       url(${interThinWoff}) format("woff");
   }
+`
+
+const inter100i = `
   @font-face {
     font-family: 'Inter';
-    font-style: italic;
+    font-style: normal;
     font-weight: 100;
     font-display: swap;
-    src: url(${interThinItalicWoff2}) format("woff2"),
-      url(${interThinItalicWoff}") format("woff");
+    src: local('Inter Thin'),
+      url(${interThinItalicWoff2}) format("woff2"),
+      url(${interThinItalicWoff}) format("woff");
   }
+`
+
+const inter200 = `
   @font-face {
     font-family: 'Inter';
     font-style: normal;
@@ -66,6 +86,8 @@ const interFontFaceStack = `
     src: url(${interExtraLightWoff2}) format("woff2"),
       url(${interExtraLightWoff}) format("woff");
   }
+`
+const inter200i = `
   @font-face {
     font-family: 'Inter';
     font-style: italic;
@@ -74,6 +96,9 @@ const interFontFaceStack = `
     src: url(${interExtraLightItalicWoff2}) format("woff2"),
       url(${interExtraLightItalicWoff}) format("woff");
   }
+`
+
+const inter300 = `
   @font-face {
     font-family: 'Inter';
     font-style: normal;
@@ -82,6 +107,9 @@ const interFontFaceStack = `
     src: url(${interLightWoff2}) format("woff2"),
       url(${interLightWoff}) format("woff");
   }
+`
+
+const inter300i = `
   @font-face {
     font-family: 'Inter';
     font-style: italic;
@@ -90,6 +118,9 @@ const interFontFaceStack = `
     src: url(${interLightItalicWoff2}) format("woff2"),
       url(${interLightItalicWoff}) format("woff");
   }
+`
+
+const inter400 = `
   @font-face {
     font-family: 'Inter';
     font-style: normal;
@@ -98,6 +129,9 @@ const interFontFaceStack = `
     src: url(${interRegularWoff2}) format("woff2"),
       url(${interRegularWoff}) format("woff");
   }
+`
+
+const inter400i = `
   @font-face {
     font-family: 'Inter';
     font-style: italic;
@@ -106,6 +140,9 @@ const interFontFaceStack = `
     src: url(${interItalicWoff2}) format("woff2"),
       url(${interItalicWoff}) format("woff");
   }
+`
+
+const inter500 = `
   @font-face {
     font-family: 'Inter';
     font-style: normal;
@@ -114,6 +151,8 @@ const interFontFaceStack = `
     src: url(${interMediumWoff2}) format("woff2"),
       url(${interMediumWoff}) format("woff");
   }
+`
+const inter500i = `
   @font-face {
     font-family: 'Inter';
     font-style: italic;
@@ -122,6 +161,9 @@ const interFontFaceStack = `
     src: url(${interMediumItalicWoff2}) format("woff2"),
       url(${interMediumItalicWoff}) format("woff");
   }
+`
+
+const inter600 = `
   @font-face {
     font-family: 'Inter';
     font-style: normal;
@@ -130,6 +172,9 @@ const interFontFaceStack = `
     src: url(${interSemiBoldWoff2}) format("woff2"),
       url(${interSemiBoldWoff}) format("woff");
   }
+`
+
+const inter600i = `
   @font-face {
     font-family: 'Inter';
     font-style: italic;
@@ -138,6 +183,9 @@ const interFontFaceStack = `
     src: url(${interSemiBoldItalicWoff2}) format("woff2"),
       url(${interSemiBoldItalicWoff}) format("woff");
   }
+`
+
+const inter700 = `
   @font-face {
     font-family: 'Inter';
     font-style: normal;
@@ -146,6 +194,9 @@ const interFontFaceStack = `
     src: url(${interBoldWoff2}) format("woff2"),
       url(${interBoldWoff}) format("woff");
   }
+`
+
+const inter700i = `
   @font-face {
     font-family: 'Inter';
     font-style: italic;
@@ -154,6 +205,8 @@ const interFontFaceStack = `
     src: url(${interBoldItalicWoff2}) format("woff2"),
       url(${interBoldItalicWoff}) format("woff");
   }
+`
+const inter800 = `
   @font-face {
     font-family: 'Inter';
     font-style: normal;
@@ -162,6 +215,9 @@ const interFontFaceStack = `
     src: url(${interExtraBoldWoff2}) format("woff2"),
       url(${interExtraBoldWoff}) format("woff");
   }
+`
+
+const inter800i = `
   @font-face {
     font-family: 'Inter';
     font-style: italic;
@@ -170,6 +226,9 @@ const interFontFaceStack = `
     src: url(${interExtraBoldItalicWoff2}) format("woff2"),
       url(${interExtraBoldItalicWoff}) format("woff");
   }
+`
+
+const inter900 = `
   @font-face {
     font-family: 'Inter';
     font-style: normal;
@@ -178,6 +237,8 @@ const interFontFaceStack = `
     src: url(${interBlackWoff2}) format("woff2"),
       url(${interBlackWoff}) format("woff");
   }
+`
+const inter900i = `
   @font-face {
     font-family: 'Inter';
     font-style: italic;
@@ -186,6 +247,9 @@ const interFontFaceStack = `
     src: url(${interBlackItalicWoff2}) format("woff2"),
       url(${interBlackItalicWoff}) format("woff");
   }
+`
+
+const interVariable = `
   @font-face {
     font-family: 'Inter var';
     font-weight: 100 900;
@@ -194,6 +258,9 @@ const interFontFaceStack = `
     font-named-instance: 'Regular';
     src: url(${interRomanVarWoff2}) format("woff2");
   }
+`
+
+const interVariableItalic = `
   @font-face {
     font-family: 'Inter var';
     font-weight: 100 900;
@@ -203,13 +270,39 @@ const interFontFaceStack = `
     src: url(${interItalicVarWoff2}) format("woff2");
   }
 `
-// Inter var experimental
-// @font-face {
-//   font-family: 'Inter var experimental';
-//   font-weight: 100 900;
-//   font-display: swap;
-//   font-style: oblique 0deg 10deg;
-//   src: url(${interVarWoff2}) format("woff2");
-// }
+
+const interVariableExperimental = `
+  @font-face {
+    font-family: 'Inter var experimental';
+    font-weight: 100 900;
+    font-display: swap;
+    font-style: oblique 0deg 10deg;
+    src: url(${interVarWoff2}) format("woff2");
+  }
+`
+
+const interFontFaceStack = (
+  inter100 +
+  inter100i +
+  inter200 +
+  inter200i +
+  inter300 +
+  inter300i +
+  inter400 +
+  inter400i +
+  inter500 +
+  inter500i +
+  inter600 +
+  inter600i +
+  inter700 +
+  inter700i +
+  inter800 +
+  inter800i +
+  inter900 +
+  inter900i +
+  interVariable +
+  interVariableItalic +
+  interVariableExperimental
+)
 
 export default interFontFaceStack
