cf9ceafb0f6d122a1ab2f4870e6a05e2099b63fd
Vladimir <vladimir.skok@bvaccel.com>
2020-04-18 17:03:24 -0700

    feat: move font listener from gatsby-browser.js => Head component

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,31 +1,3 @@
 import { wrapRootElement } from './wrap-root-element'
 
-const onClientEntry = () => {
-  window.addEventListener('load', () => {
-    (() => {
-      if ("fonts" in document) {
-        // Optimization for Repeat Views
-        if (sessionStorage.fontsLoaded) {
-          document.documentElement.className += ` fonts-loaded`
-          return
-        }
-
-        Promise.all([
-          document.fonts.load(`400 1em 'Inter var'`),
-          document.fonts.load(`italic 400 1em 'Inter var'`),
-          document.fonts.load(`400 1em 'Fira Code VF'`)
-        ]).then(() => {
-          document.documentElement.className += ` fonts-loaded`
-
-          // Optimization for Repeat Views
-          sessionStorage.fontsLoaded = true
-        })
-      }
-    })()
-  })
-}
-
-export {
-  wrapRootElement,
-  onClientEntry
-}
+export { wrapRootElement }
--- src/components/head.js
+++ src/components/head.js
@@ -80,6 +80,37 @@ export default props => {
           }
         `}
       </style>
+      <script>
+        {`
+          window.addEventListener('load', () => {
+            (() => {
+              if ('fonts' in document) {
+                // Optimization for Repeat Views
+                if (sessionStorage.fontsLoaded) {
+                  console.log('sessionStorage is not empty')
+                  document.documentElement.classList.add('fonts-loaded')
+                  return
+                }
+
+                Promise.all([
+                  document.fonts.load('400 1em "Inter var"'),
+                  document.fonts.load('italic 400 1em "Inter var"'),
+                  document.fonts.load('400 1em "Fira Code VF"')
+                ]).then(() => {
+                  document.documentElement.classList.add('fonts-loaded')
+
+                  // Optimization for Repeat Views
+                  sessionStorage.fontsLoaded = true
+                })
+              }
+            })()
+          })
+
+          if (sessionStorage.fontsLoaded) {
+            document.documentElement.classList.add('fonts-loaded')
+          }
+        `}
+      </script>
     </Helmet>
   )
 }
