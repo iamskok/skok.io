553e47c506ecfbf13938824ef886d5d798806d65
Vladimir <vladimir.skok@bvaccel.com>
2020-04-25 17:16:27 -0700

    feat: add vertical rythm for LineNumber component

--- src/components/code-block/line-number.js
+++ src/components/code-block/line-number.js
@@ -1,6 +1,10 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui'
 import { PrismThemeConsumer } from './prism-theme-provider'
+import { baseThemeSettings } from '../../gatsby-plugin-theme-ui'
+import unitless from '../../utils/unitless'
+
+const { rythm } = baseThemeSettings
 
 const LineNumber = ({ index, lineNumbers, highlight = false }) => (
   <PrismThemeConsumer>
@@ -20,8 +24,8 @@ const LineNumber = ({ index, lineNumbers, highlight = false }) => (
           prismTheme.lineNumber.color
         ) ||
         prismTheme.plain.color,
-        marginLeft: -20,
-        marginRight: 20,
+        marginLeft: unitless(rythm) * -1 + `rem`,
+        marginRight: rythm,
         transition: 'background-color 400ms ease, color 400ms ease, transform 400ms ease',
         transform: lineNumbers ? 'translate3d(0, 0, 0)' : 'translate3d(-40px, 0, 0)',
       }}>
