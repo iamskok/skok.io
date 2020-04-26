31b8d073f3ba5231729c4ff1295af9f262b124fd
Vladimir <vladimir.skok@bvaccel.com>
2020-04-26 02:09:31 -0700

    fix: remove obilique style from Amstelvar @font-face

--- src/fonts/amstelvar/index.js
+++ src/fonts/amstelvar/index.js
@@ -5,7 +5,6 @@ const amstelvarFontFace = `
     font-family: 'Amstelvar';
     font-weight: 100 900;
     font-display: swap;
-    font-style: oblique 0deg 10deg;
     src: url(${amstelvar}) format('woff2-variations'),
       url(${amstelvar}) format('woff2');
   }
