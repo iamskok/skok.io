7c5c17a369c0aba2060dde51ae2ad233ed768bd7
Vladimir <vladimir.skok@bvaccel.com>
2020-04-25 17:20:20 -0700

    feat: update default component styles

--- src/gatsby-plugin-theme-ui/styles.js
+++ src/gatsby-plugin-theme-ui/styles.js
@@ -1,118 +1,131 @@
+import { baseThemeSettings } from './index'
+import unitless from '../utils/unitless'
+
+const { rythm, lineHeight: baseLineHeight } = baseThemeSettings
+
 const heading = {
-  fontWeight: 'heading',
-  lineHeight: 'heading',
-  mt: 4,
-  mb: 3,
+  fontWeight: `heading`,
+  lineHeight: `heading`,
+  marginTop: (unitless(rythm) * 3) + `rem`,
+  marginBottom: rythm,
 }
 
-const fontFamilyMonospace = {
-  fontFamily: 'monospace',
+const monospaceFont = {
+  fontFamily: `monospace`,
   fontFeatureSettings: `'salt', 'calt', 'case', 'cpsp', 'ss01', 'ss02', 'ss03', 'ss04', 'ss05', 'ss06'`,
-}
-
-const fontFamilyBody = {
-  fontFamily: 'body',
-  fontFeatureSettings: `'kern', 'calt', 'ss01', 'ss02', 'ss03'`,
 }
 
 export default {
   root: {
     margin: 0,
-    ...fontFamilyBody,
+    fontFamily: `body`,
+    fontFeatureSettings: `'kern', 'calt', 'ss01', 'ss02', 'ss03'`,
   },
   pre: {
+    margin: `0 0 ${rythm} 0`,
     fontSize: 3,
-    lineHeight: 1.5,
-    color: 'secondary',
-    bg: 'muted',
-    overflowX: 'auto',
-    ...fontFamilyMonospace,
+    lineHeight: baseLineHeight,
+    color: `secondary`,
+    bg: `muted`,
+    overflowX: `auto`,
+    ...monospaceFont,
   },
   code: {
-    float: 'left',
-    minWidth: '100%',
+    float: `left`,
+    minWidth: `100%`,
     whiteSpace: `pre`,
     paddingY: 0,
-    ...fontFamilyMonospace,
+    ...monospaceFont,
   },
   inlineCode: {
     fontSize: 3,
-    lineHeight: 1.5,
-    color: 'secondary',
-    backgroundColor: 'muted',
-    ...fontFamilyMonospace,
+    lineHeight: baseLineHeight,
+    color: `secondary`,
+    backgroundColor: `muted`,
+    ...monospaceFont,
   },
   h1: {
     ...heading,
-    fontSize: [5, 6],
+    fontSize: 5,
+    lineHeight: 5
   },
   h2: {
     ...heading,
-    fontSize: [4, 5],
+    fontSize: 4,
+    lineHeight: 4,
   },
   h3: {
     ...heading,
     fontSize: 3,
+    lineHeight: 3,
   },
   h4: {
     ...heading,
     fontSize: 2,
+    lineHeight: 2,
   },
   h5: {
     ...heading,
     fontSize: 1,
+    lineHeight: 1,
   },
   h6: {
     ...heading,
     fontSize: 0,
+    lineHeight: 0,
   },
   p: {
-    fontSize: [2, 3],
-    m: 0,
-    mb: 4,
+    fontSize: 3,
+    margin: `0 0 ${rythm} 0`,
+    lineHeight: baseLineHeight,
+    '& > img': {
+      marginBottom: 0
+    },
   },
   a: {
-    color: 'primary',
+    color: `primary`,
     '&:hover': {
-      color: 'secondary',
+      color: `secondary`,
+      textDecoration: `none`,
     }
   },
   img: {
-    maxWidth: '100%',
-    height: 'auto',
+    marginBottom: rythm,
+    maxWidth: `100%`,
+    height: `auto`,
   },
   hr: {
-    mt: 5,
-    mb: 5,
+    marginTop: (unitless(rythm) * 3) + `rem`,
+    marginBottom: (unitless(rythm) * 3) + `rem`,
     border: 0,
-    borderBottom: '1px solid',
+    borderBottom: `1px solid`,
   },
   table: {
-    width: '100%',
-    my: 4,
-    borderCollapse: 'separate',
+    width: `100%`,
+    marginY: 4,
+    borderCollapse: `separate`,
     borderSpacing: 0,
   },
   th: {
-    textAlign: 'left',
-    verticalAlign: 'bottom',
-    paddingTop: '4px',
-    paddingBottom: '4px',
-    paddingRight: '4px',
+    textAlign: `left`,
+    verticalAlign: `bottom`,
+    paddingTop: `4px`,
+    paddingBottom: `4px`,
+    paddingRight: `4px`,
     paddingLeft: 0,
-    borderColor: 'inherit',
-    borderBottomWidth: '2px',
-    borderBottomStyle: 'solid'
+    borderColor: `inherit`,
+    borderBottomWidth: `2px`,
+    borderBottomStyle: `solid`
   },
   td: {
-    textAlign: 'left',
-    verticalAlign: 'top',
-    paddingTop: '4px',
-    paddingBottom: '4px',
-    paddingRight: '4px',
+    textAlign: `left`,
+    verticalAlign: `top`,
+    paddingTop: `4px`,
+    paddingBottom: `4px`,
+    paddingRight: `4px`,
     paddingLeft: 0,
-    borderColor: 'inherit',
-    borderBottomWidth: '1px',
-    borderBottomStyle: 'solid'
+    borderColor: `inherit`,
+    borderBottomWidth: `1px`,
+    borderBottomStyle: `solid`
   },
 }
