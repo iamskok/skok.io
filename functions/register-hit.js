ebb3ee7202703fab75f13d5f9107ae843997a099
Vladimir Skok <skok@vova.io>
2020-10-10 17:55:10 -0400

    style: disabble eslint for a console.log

--- functions/register-hit.js
+++ functions/register-hit.js
@@ -8,14 +8,26 @@ const {
   FIREBASE_AUTH_DOMAIN,
   FIREBASE_DATABASE_URL,
   FIREBASE_PROJECT_ID,
+
+  FIREBASE_API_KEY_DEV,
+  FIREBASE_AUTH_DOMAIN_DEV,
+  FIREBASE_DATABASE_URL_DEV,
+  FIREBASE_PROJECT_ID_DEV,
 } = process.env
 
-const config = {
-  apiKey: FIREBASE_API_KEY,
-  authDomain: FIREBASE_AUTH_DOMAIN,
-  databaseURL: FIREBASE_DATABASE_URL,
-  projectId: FIREBASE_PROJECT_ID,
-}
+const config = process.env.NETLIFY_DEV
+  ? {
+      apiKey: FIREBASE_API_KEY_DEV,
+      authDomain: FIREBASE_AUTH_DOMAIN_DEV,
+      databaseURL: FIREBASE_DATABASE_URL_DEV,
+      projectId: FIREBASE_PROJECT_ID_DEV,
+    }
+  : {
+      apiKey: FIREBASE_API_KEY,
+      authDomain: FIREBASE_AUTH_DOMAIN,
+      databaseURL: FIREBASE_DATABASE_URL,
+      projectId: FIREBASE_PROJECT_ID,
+    }
 
 if (!firebase.apps.length) {
   firebase.initializeApp(config)
@@ -24,13 +36,23 @@ if (!firebase.apps.length) {
 const db = firebase.firestore()
 
 exports.handler = async event => {
+  console.log("NETLIFY_DEV", process.env.NETLIFY_DEV) // eslint-disable-line
+
   const { slug } = event.queryStringParameters
 
-  const postRef = db.doc(`posts/${slug}`)
-  const increment = firebase.firestore.FieldValue.increment(1)
-  postRef.update({ hits: increment })
+  const postRef = db.collection(`posts`).doc(slug)
+  const postSnapshot = await postRef.get()
 
-  const { hits: registeredHits } = (await db.doc(`posts/${slug}`).get()).data()
+  if (postSnapshot.exists) {
+    const increment = firebase.firestore.FieldValue.increment(1)
+    postRef.update({ hits: increment })
+  } else {
+    postRef.set({
+      hits: 1,
+    })
+  }
+
+  const { hits: registeredHits } = (await postRef.get()).data()
 
   return {
     statusCode: 200,
--- src/components/HitCounter/index.js
+++ src/components/HitCounter/index.js
@@ -1,43 +1,10 @@
 import React, { useState, useEffect } from "react"
 import firebase from "firebase"
 
-const FIREBASE_API_KEY = ``
-const FIREBASE_AUTH_DOMAIN = ``
-const FIREBASE_DATABASE_URL = ``
-const FIREBASE_PROJECT_ID = ``
-
-const config = {
-  apiKey: FIREBASE_API_KEY,
-  authDomain: FIREBASE_AUTH_DOMAIN,
-  databaseURL: FIREBASE_DATABASE_URL,
-  projectId: FIREBASE_PROJECT_ID,
-}
-
-firebase.initializeApp(config)
-
-const db = firebase.firestore()
-
 const HitCounter = ({ slug }) => {
   const [hits, setHits] = useState(0)
 
-  useEffect(() => {
-    const getHits = async () => {
-      try {
-        db.doc(`posts/${slug}`).onSnapshot(doc => {
-          const { hits: registeredHits } = doc.data()
-
-          setHits(registeredHits)
-        })
-
-        fetch(`/.netlify/functions/register-hit?slug=${slug}`)
-      } catch (error) {
-        throw new Error(error)
-      }
-    }
-    getHits()
-  }, [slug])
-
-  return <h1>HitCounter: {hits > 1 ? hits : null}</h1>
+  return <h1>HitCounter: {hits}</h1>
 }
 
 export default HitCounter
