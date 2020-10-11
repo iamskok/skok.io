db58cb9a3402ca910492dc81e6758a88ca9f8972
Vladimir Skok <skok@vova.io>
2020-10-10 20:05:22 -0400

    feat: configure env variables for firestore

--- src/components/HitCounter/index.js
+++ src/components/HitCounter/index.js
@@ -1,19 +1,12 @@
 import React, { useState, useEffect } from "react"
 import firebase from "firebase"
 
-const FIREBASE_API_KEY = ``
-const FIREBASE_AUTH_DOMAIN = ``
-const FIREBASE_DATABASE_URL = ``
-const FIREBASE_PROJECT_ID = ``
-
-const config = {
-  apiKey: FIREBASE_API_KEY,
-  authDomain: FIREBASE_AUTH_DOMAIN,
-  databaseURL: FIREBASE_DATABASE_URL,
-  projectId: FIREBASE_PROJECT_ID,
-}
-
-firebase.initializeApp(config)
+firebase.initializeApp({
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+})
 
 const db = firebase.firestore()
 
