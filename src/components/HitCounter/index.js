9f674d0a7167aa466e29d1704125a5f966302c48
Vladimir Skok <skok@vova.io>
2020-10-11 13:00:03 -0400

    feat: check if doc exists in firestore

--- src/components/HitCounter/index.js
+++ src/components/HitCounter/index.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react"
 import firebase from "firebase"
 
-firebase.initializeApp({
+const config = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.FIREBASE_DATABASE_URL,
   projectId: process.env.FIREBASE_PROJECT_ID,
-})
+}
+
+firebase.initializeApp(config)
 
 const db = firebase.firestore()
 
@@ -14,20 +16,38 @@ const HitCounter = ({ slug }) => {
   const [hits, setHits] = useState(0)
 
   useEffect(() => {
+    let unsubscribeFromPostUpdates
+
     const getHits = async () => {
       try {
-        db.doc(`posts/${slug}`).onSnapshot(doc => {
-          const { hits: registeredHits } = doc.data()
+        const postRef = db.collection(`posts`).doc(slug)
+        const postDoc = await postRef.get()
+        const isPostDocExists = postDoc.exists
 
-          setHits(registeredHits)
+        const subscribeOnPostUpdates = postRef => {
+          return postRef.onSnapshot(doc => {
+            const { hits: registeredHits } = doc.data()
+
+            setHits(registeredHits)
+          })
+        }
+
+        if (isPostDocExists) {
+          unsubscribeFromPostUpdates = subscribeOnPostUpdates(postRef)
+        }
+
+        fetch(`/.netlify/functions/register-hit?slug=${slug}`).then(() => {
+          if (!isPostDocExists) {
+            unsubscribeFromPostUpdates = subscribeOnPostUpdates(postRef)
+          }
         })
-
-        fetch(`/.netlify/functions/register-hit?slug=${slug}`)
       } catch (error) {
         throw new Error(error)
       }
     }
     getHits()
+
+    return () => unsubscribeFromPostUpdates()
   }, [slug])
 
   return <h1>HitCounter: {hits}</h1>
