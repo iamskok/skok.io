dd7459933a7b48bb8daf3f40b42a1c3bee017604
Vladimir Skok <skok@vova.io>
2020-10-28 21:58:01 -0400

    refactor: open graph component

--- src/components/Head/OpenGraph.js
+++ src/components/Head/OpenGraph.js
@@ -4,6 +4,7 @@ import { Helmet } from "react-helmet"
 const OpenGraph = ({
   title,
   description,
+  locale,
   publishedTime,
   image,
   imageAlt,
@@ -12,28 +13,25 @@ const OpenGraph = ({
   firstName,
   lastName,
   isBlogPost,
-}) => {
-  return (
-    <Helmet>
-      <meta property="og:site_name" content={siteName} />
-      <meta property="og:url" content={url} />
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
-      <meta property="og:type" content={isBlogPost ? `article` : `website`} />
-      {isBlogPost && (
-        <meta property="article:published_time" content={publishedTime} />
-      )}
-      {isBlogPost && (
-        <meta property="article:author" content={`${firstName} ${lastName}`} />
-      )}
-      {!isBlogPost && (
-        <meta property="profile:first_name" content={firstName} />
-      )}
-      {!isBlogPost && <meta property="profile:last_name" content={lastName} />}
-      <meta property="og:image" content={image} />
-      <meta property="og:image:alt" content={imageAlt} />
-    </Helmet>
-  )
-}
+}) => (
+  <Helmet>
+    <meta property="og:site_name" content={siteName} />
+    <meta property="og:url" content={url} />
+    <meta property="og:locale" content={locale} />
+    <meta property="og:title" content={title} />
+    <meta property="og:description" content={description} />
+    <meta property="og:type" content={isBlogPost ? `article` : `website`} />
+    {isBlogPost && (
+      <meta property="article:published_time" content={publishedTime} />
+    )}
+    {isBlogPost && (
+      <meta property="article:author" content={`${firstName} ${lastName}`} />
+    )}
+    {!isBlogPost && <meta property="profile:first_name" content={firstName} />}
+    {!isBlogPost && <meta property="profile:last_name" content={lastName} />}
+    <meta property="og:image" content={image} />
+    <meta property="og:image:alt" content={imageAlt} />
+  </Helmet>
+)
 
 export default OpenGraph
