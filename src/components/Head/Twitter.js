9a53c7641f999da737434236614a5bc3568e9b91
Vladimir Skok <skok@vova.io>
2020-10-28 21:57:39 -0400

    refactor: twitter component

--- src/components/Head/Twitter.js
+++ src/components/Head/Twitter.js
@@ -1,17 +1,15 @@
 import React from "react"
 import { Helmet } from "react-helmet"
 
-const Twitter = ({ title, description, image, imageAlt, twitterHandle }) => {
-  return (
-    <Helmet>
-      <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:title" content={title} />
-      <meta property="twitter:description" content={description} />
-      <meta name="twitter:creator" content={twitterHandle} />
-      <meta property="twitter:image" content={image} />
-      <meta property="twitter:image:alt" content={imageAlt} />
-    </Helmet>
-  )
-}
+const Twitter = ({ title, description, image, imageAlt, creator }) => (
+  <Helmet>
+    <meta property="twitter:card" content="summary_large_image" />
+    <meta property="twitter:title" content={title} />
+    <meta property="twitter:description" content={description} />
+    <meta property="twitter:image" content={image} />
+    <meta property="twitter:image:alt" content={imageAlt} />
+    <meta name="twitter:creator" content={creator} />
+  </Helmet>
+)
 
 export default Twitter
