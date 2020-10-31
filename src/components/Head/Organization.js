04719cbb88b6636fdefc37ccca3a3acf91b56f76
Vladimir Skok <skok@vova.io>
2020-10-30 22:38:47 -0400

    feat: add Organization JSON-LD markup

---
+++ src/components/Head/Organization.js
@@ -0,0 +1,64 @@
+import React from "react"
+import { Helmet } from "react-helmet"
+
+const Person = ({
+  name,
+  founder,
+  telephone,
+  email,
+  image,
+  url,
+  logo,
+  sameAs,
+  description,
+}) => {
+  const schema = Object.assign(
+    {
+      "@context": `http://schema.org`,
+      "@id": "#organization",
+      "@type": `Organization`,
+      address: {
+        "@type": `PostalAddress`,
+        "@id": `#address`,
+      },
+      founder: {
+        "@type": `Person`,
+        "@id": `#person`,
+      },
+    },
+    description ? { description } : null,
+    email ? { email } : null,
+    telephone ? { telephone } : null,
+    name ? { name } : null,
+    url ? { url } : null,
+    Object.values(sameAs).length > 0 ? { sameAs: Object.values(sameAs) } : null,
+    image
+      ? {
+          image: {
+            "@type": `ImageObject`,
+            height: 2048,
+            url: image,
+            width: 2048,
+          },
+        }
+      : null,
+    logo
+      ? {
+          logo: {
+            "@type": `ImageObject`,
+            height: 60,
+            url: logo,
+            width: 600,
+          },
+        }
+      : null
+  )
+
+  return (
+    <Helmet>
+      <script type="application/ld+json">{JSON.stringify(schema)}</script>
+    </Helmet>
+  )
+}
+
+export default Person
