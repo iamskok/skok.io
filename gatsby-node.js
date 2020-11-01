cfc1528d85212073e0b5d7cb39c0299d0a8382d7
Vladimir Skok <skok@vova.io>
2020-11-01 03:13:16 -0500

    feat: remove /blog from blog post URLs

--- gatsby-node.js
+++ gatsby-node.js
@@ -13,7 +13,7 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
 
     createNodeField({
       name: `slug`,
-      value: `/blog${value}`,
+      value,
       node,
     })
   }
@@ -62,10 +62,12 @@ const createBlogPosts = (createPage, edges) => {
   edges.forEach(({ node }, i) => {
     const prev = i === 0 ? null : edges[i - 1].node
     const next = i === edges.length - 1 ? null : edges[i + 1].node
-    const slug = node.fields.slug.replace(`/blog/`, ``).replace(`/`, ``)
+    const {
+      fields: { slug },
+    } = node
 
     createPage({
-      path: node.fields.slug,
+      path: slug,
       component: path.resolve(`src/templates/BlogPost/index.js`),
       context: {
         id: node.id,
