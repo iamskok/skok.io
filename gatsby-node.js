a0c673770847370c17c456374f1e7d18ee02c8c1
Vladimir Skok <skok@vova.io>
2020-10-31 23:23:54 -0400

    style: add extra new line

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,6 @@
 const { createFilePath } = require("gatsby-source-filesystem")
 const path = require("path")
+
 const PAGINATION_OFFSET = 8
 
 // Here we're adding extra stuff to the "node" (like the slug)
