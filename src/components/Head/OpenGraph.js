925b5be51459b17c0a283f8e09d77232c382e3bb
Vladimir Skok <skok@vova.io>
2020-11-05 23:44:46 -0500

    feat: do not output meta tags if there is no value

--- src/components/Head/OpenGraph.js
+++ src/components/Head/OpenGraph.js
@@ -16,11 +16,11 @@ const OpenGraph = ({
   isArticle,
 }) => (
   <Helmet>
-    <meta property="og:site_name" content={siteName} />
-    <meta property="og:url" content={url} />
-    <meta property="og:locale" content={locale} />
-    <meta property="og:title" content={title} />
-    <meta property="og:description" content={description} />
+    {siteName && <meta property="og:site_name" content={siteName} />}
+    {url && <meta property="og:url" content={url} />}
+    {locale && <meta property="og:locale" content={locale} />}
+    {title && <meta property="og:title" content={title} />}
+    {description && <meta property="og:description" content={description} />}
     <meta property="og:type" content={isArticle ? `article` : `website`} />
     {isArticle && (
       <meta property="article:published_time" content={publishedTime} />
@@ -30,8 +30,8 @@ const OpenGraph = ({
     )}
     {!isArticle && <meta property="profile:first_name" content={firstName} />}
     {!isArticle && <meta property="profile:last_name" content={lastName} />}
-    <meta property="og:image" content={image} />
-    <meta property="og:image:alt" content={imageAlt} />
+    {image && <meta property="og:image" content={image} />}
+    {imageAlt && <meta property="og:image:alt" content={imageAlt} />}
     {Object.values(seeAlso).length > 0 &&
       Object.values(seeAlso).map((url, index) => (
         <meta key={index} property="og:see_also" content={url} />
