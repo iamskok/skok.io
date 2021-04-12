8201d005cffce9d4d3110079bc761e68a81c7189
Vladimir Skok <skok@vova.io>
2021-04-11 20:34:59 -0400

    feat: add font face observer

--- src/components/Annotation/index.js
+++ src/components/Annotation/index.js
@@ -1,11 +1,38 @@
 /** @jsx jsx */
+import { useState, useEffect } from "react"
 import { jsx, useThemeUI } from "theme-ui"
 import { RoughNotation } from "react-rough-notation"
 import { useInView } from "react-intersection-observer"
+import FontFaceObserver from "fontfaceobserver"
 
-const options = {
+const inViewOptions = {
   threshold: 0.1,
 }
+
+const RoughNotationOptions = {
+  multiline: true,
+  animationDelay: 200,
+  animationDuration: 1200,
+}
+
+const fontFaces = [
+  {
+    font: `Amstelvar`,
+    style: `italic`,
+  },
+  {
+    font: `Dank Mono`,
+    style: `italic`,
+  },
+  {
+    font: `Dank Mono`,
+    style: `normal`,
+  },
+  {
+    font: `Inter Var`,
+    style: ``,
+  },
+]
 
 const Underline = props => {
   const {
@@ -13,16 +40,25 @@ const Underline = props => {
       colors: { primary },
     },
   } = useThemeUI()
-  const { ref, inView } = useInView(options)
+  const { ref, inView } = useInView(inViewOptions)
+  const [show, setShow] = useState()
+
+  useEffect(() => {
+    Promise.all(
+      fontFaces.map(({ font, style }) =>
+        new FontFaceObserver(font, { style }).load()
+      )
+    ).then(() => setShow(inView))
+  }, [inView])
 
   return (
     <span ref={ref}>
       <RoughNotation
         type="underline"
-        show={inView}
+        show={show}
         color={primary}
-        multiline={true}
         iterations={5}
+        {...RoughNotationOptions}
         {...props}
       />
     </span>
@@ -35,99 +71,148 @@ const Box = props => {
       colors: { primary },
     },
   } = useThemeUI()
-  const { ref, inView } = useInView(options)
+  const { ref, inView } = useInView(inViewOptions)
+  const [show, setShow] = useState()
+
+  useEffect(() => {
+    Promise.all(
+      fontFaces.map(({ font, style }) =>
+        new FontFaceObserver(font, { style }).load()
+      )
+    ).then(() => setShow(inView))
+  }, [inView])
 
   return (
     <span ref={ref}>
       <RoughNotation
         type="box"
-        show={inView}
+        show={show}
         color={primary}
-        multiline={true}
         iterations={3}
+        {...RoughNotationOptions}
         {...props}
       />
     </span>
   )
 }
+
 const Circle = props => {
   const {
     theme: {
       colors: { primary },
     },
   } = useThemeUI()
-  const { ref, inView } = useInView(options)
+  const { ref, inView } = useInView(inViewOptions)
+  const [show, setShow] = useState()
+
+  useEffect(() => {
+    Promise.all(
+      fontFaces.map(({ font, style }) =>
+        new FontFaceObserver(font, { style }).load()
+      )
+    ).then(() => setShow(inView))
+  }, [inView])
 
   return (
     <span ref={ref}>
       <RoughNotation
         type="circle"
-        show={inView}
+        show={show}
         color={primary}
-        multiline={true}
         iterations={5}
+        {...RoughNotationOptions}
         {...props}
       />
     </span>
   )
 }
+
 const Highlight = props => {
   const {
     theme: {
       colors: { primary },
     },
   } = useThemeUI()
-  const { ref, inView } = useInView(options)
+  const { ref, inView } = useInView(inViewOptions)
+  const [show, setShow] = useState()
+
+  useEffect(() => {
+    Promise.all(
+      fontFaces.map(({ font, style }) =>
+        new FontFaceObserver(font, { style }).load()
+      )
+    ).then(() => setShow(inView))
+  }, [inView])
 
   return (
     <span ref={ref}>
       <RoughNotation
         type="highlight"
-        show={inView}
+        show={show}
         color={primary}
-        multiline={true}
+        {...RoughNotationOptions}
         {...props}
       />
     </span>
   )
 }
+
 const StrikeThrough = props => {
   const {
     theme: {
       colors: { primary },
     },
   } = useThemeUI()
-  const { ref, inView } = useInView(options)
+  const { ref, inView } = useInView(inViewOptions)
+  const [show, setShow] = useState()
+
+  useEffect(() => {
+    Promise.all(
+      fontFaces.map(({ font, style }) =>
+        new FontFaceObserver(font, { style }).load()
+      )
+    ).then(() => setShow(inView))
+  }, [inView])
 
   return (
     <span ref={ref}>
       <RoughNotation
         type="strike-through"
-        show={inView}
+        show={show}
         color={primary}
-        multiline={true}
         iterations={5}
+        {...RoughNotationOptions}
         {...props}
       />
     </span>
   )
 }
+
 const CrossedOff = props => {
   const {
     theme: {
       colors: { primary },
     },
   } = useThemeUI()
-  const { ref, inView } = useInView(options)
+  const { ref, inView } = useInView(inViewOptions)
+  const [show, setShow] = useState()
+
+  useEffect(() => {
+    Promise.all(
+      fontFaces.map(({ font, style }) =>
+        new FontFaceObserver(font, { style }).load()
+      )
+    ).then(() => setShow(inView))
+  }, [inView])
 
   return (
     <span ref={ref}>
       <RoughNotation
         type="crossed-off"
-        show={inView}
+        show={show}
         color={primary}
-        multiline={true}
         iterations={5}
+        {...RoughNotationOptions}
         {...props}
       />
     </span>
