d1cafee72e2cf0f2bf42665a7097ad2a0232a110
Vladimir Skok <skok@vova.io>
2021-05-01 12:32:01 -0400

    refactor: edit handleProgress

--- src/templates/Article/handleProgress.js
+++ src/templates/Article/handleProgress.js
@@ -5,12 +5,11 @@ const handleProgress = ({ headerRef, setProgress }) => {
   const pageTotalHeight = document.body.scrollHeight
   const pageInnerHeight = window.innerHeight
 
-  const progress =
-    pageScrollOffset < headerHeight
-      ? 0
-      : pageScrollOffset / (pageTotalHeight - pageInnerHeight)
+  // Show progress when scrolled past `Header` component
+  const isVisible = pageScrollOffset > headerHeight
+  const progress = pageScrollOffset / (pageTotalHeight - pageInnerHeight)
 
-  setProgress(progress)
+  setProgress(isVisible ? progress : 0)
 }
 
 export default handleProgress
