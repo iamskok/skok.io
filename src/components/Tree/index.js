2bde68fa8e2e9b8e12860a0a5c7a76173a43dd1c
Vladimir Skok <iamskok@users.noreply.github.com>
2021-05-03 22:05:56 -0400

    fix: edit Tree import paths

--- src/components/Tree/index.js
+++ src/components/Tree/index.js
@@ -1,7 +1,7 @@
 /** @jsx jsx */
 import { jsx, Box } from "theme-ui"
-import File from "./file"
-import Folder from "./folder"
+import File from "./File"
+import Folder from "./Folder"
 
 const Tree = ({ children }) => <Box>{children}</Box>
 
