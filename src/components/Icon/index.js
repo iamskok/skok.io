3f9637fc3fb3bf0b8c08f01e009b0edb9c54fdc1
Vladimir Skok <skok@vova.io>
2021-06-19 19:36:06 -0400

    feat: add Icon component and theme ui variants

---
+++ src/components/Icon/index.js
@@ -0,0 +1,22 @@
+import React from "react"
+import { Box } from "theme-ui"
+
+// Currently Theme UI is not exposing its SVG component
+// https://github.com/system-ui/theme-ui/blob/develop/packages/components/src/SVG.js
+const Icon = React.forwardRef(function Icon(props, ref) {
+  return (
+    <Box
+      ref={ref}
+      as="svg"
+      variant="default"
+      viewBox="0 0 24 24"
+      {...props}
+      __themeKey="icons"
+      __css={{
+        fill: `currentColor`,
+      }}
+    />
+  )
+})
+
+export default Icon
