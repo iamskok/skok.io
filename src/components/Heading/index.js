094d8d70921cb94bfaf95d45fb41817fcb47f805
Vladimir Skok <skok@vova.io>
2021-06-20 04:06:50 -0400

    refactor: edit Heading component

--- src/components/Heading/index.js
+++ src/components/Heading/index.js
@@ -1,34 +1,35 @@
 /** @jsx jsx */
 import { jsx } from "theme-ui"
 
-/* eslint react/display-name: 0 */
-/* eslint react-hooks/rules-of-hooks: 0 */
-const Heading = Tag => props => {
-  const { id, children } = props
+/* eslint-disable react/display-name */
+const Heading =
+  Tag =>
+  ({ id, children, ...rest }) => {
+    if (!id) {
+      return <Tag {...rest}>{children}</Tag>
+    }
 
-  if (!id) return <Tag {...props} />
-
-  return (
-    <Tag sx={{ scrollMarginTop: 3 }} {...props}>
-      <a
-        href={`#${id}`}
-        sx={{
-          textDecoration: `none`,
-          fontFamily: `body`,
-          marginRight: 2,
-          borderRadius: 1,
-          color: `primary`,
-          transition: `heading`,
-          "&:hover": {
-            color: `secondary`,
-          },
-        }}
-      >
-        #
-      </a>
-      {children}
-    </Tag>
-  )
-}
+    return (
+      <Tag id={id} sx={{ scrollMarginTop: 3 }} {...rest}>
+        <a
+          href={`#${id}`}
+          sx={{
+            textDecoration: `none`,
+            fontFamily: `body`,
+            marginRight: 2,
+            borderRadius: 1,
+            color: `primary`,
+            transition: `heading`,
+            "&:hover": {
+              color: `secondary`,
+            },
+          }}
+        >
+          #
+        </a>
+        {children}
+      </Tag>
+    )
+  }
 
 export default Heading
