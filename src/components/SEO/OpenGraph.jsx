9248c923d2e1b529a9d024ad51ec83c24b21b230
Vladimir Skok <skok@vova.io>
2021-07-03 00:14:39 -0400

    fix: edit OpenGraph markup

--- src/components/SEO/OpenGraph.jsx
+++ src/components/SEO/OpenGraph.jsx
@@ -12,11 +12,11 @@ const OpenGraph = ({
   dateModified,
   datePublished,
   language,
-  activePages: { isArticle },
+  activePages: { isPost },
   url,
 }) => (
   <Helmet>
-    <meta property="og:type" content={isArticle ? `article` : `website`} />
+    <meta property="og:type" content={isPost ? `article` : `website`} />
     <meta property="og:url" content={url} />
     <meta property="og:site_name" content={siteName} />
     <meta property="og:locale" content={language} />
@@ -25,13 +25,13 @@ const OpenGraph = ({
     <meta property="og:image" content={image} />
     <meta property="og:image:alt" content={imageAlt} />
 
-    {isArticle && datePublished && (
+    {isPost && datePublished && (
       <meta property="article:published_time" content={datePublished} />
     )}
-    {isArticle && dateModified && (
+    {isPost && dateModified && (
       <meta property="article:modified_time" content={dateModified} />
     )}
-    {isArticle && (
+    {isPost && (
       <meta property="article:author" content={`${firstName} ${lastName}`} />
     )}
   </Helmet>
